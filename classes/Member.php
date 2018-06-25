5e16863b2f7eec008b42a743b7346ff9096de9e2
IreneSalomo <irene.salomo@gmail.com>
2018-06-25 17:30:40 +1000

    init project

---
+++ classes/Member.php
@@ -0,0 +1,30 @@
+<?php
+    class Member{
+        public $username = "";
+        private $loggedIn = false;
+
+        public function login(){
+            $this->loggedIn = true;
+        }
+
+        public function logout(){
+            $this->loggedIn = false;
+        }
+
+        public function isLoggedIn(){
+            return $this->loggedIn;
+        }
+
+    }
+
+    $member = new Member();
+    $member2 = new Member();
+
+    $member->username = "Irene";
+    echo $member->username." is ".($member->isLoggedIn() ? "logged in" : "logged out")."</br>";
+    $member->login();
+    echo $member->username." is ".($member->isLoggedIn() ? "logged in" : "logged out")."</br>";
+    $member->logout();
+    echo $member->username." is ".($member->isLoggedIn() ? "logged in" : "logged out")."</br>";
+
+?>
